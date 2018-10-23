0fb367a492aede47d85597ec330ec2a5d33f7533
jhfaber <jfmarina@unal.edu.co>
2018-10-23 10:23:07 -0500

    Validators

--- ModelTemplatesApp/forms.py
+++ ModelTemplatesApp/forms.py
@@ -1,7 +1,29 @@
 from django import forms
+from django.core import validators
 
-class formName(forms.Form):
-    name=forms.CharField()
+def check_for_z(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError("NAME NEEDS TO START WITH Z")
+
+class FormName(forms.Form):
+    name=forms.CharField(validators=[check_for_z])
     email= forms.EmailField()
     text = forms.CharField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False,
+                                widget=forms.HiddenInput,
+                                validators=[validators.MaxLengthValidator(0)])
+
+
+
+
+
+    #Robot will be catchat with this hidden field. botcatcher
+
+    """
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        if len(botcatcher) > 0:
+            raise forms.ValidationError("GOTCHAT BOT!")
+        return botcatcher
+    """
